7ab94c3e706a21a414dbe0f49d58e45175ee0d32
Kate Ruzhnikova <kate.ruzh@yandex.ru>
2022-07-14 03:03:31 +0300

    На первое ревью 5ПР

--- scripts/index.js
+++ scripts/index.js
@@ -57,26 +57,34 @@ const popupImg = document.querySelector(selectors.popupImg);
 const figureImg = document.querySelector(selectors.figureImg);
 const figureFigcaption = document.querySelector(selectors.figureFigcaption);
 
-const templateElement = document.querySelector(selectors.templateElement).content; //Чтобы получить содержимое template, нужно обратиться к его свойству content, а дальше мы его можем клонировать
+const templateElement = document.querySelector(selectors.templateElement).content; 
 const popupCloseButtonElement = document.querySelectorAll(selectors.popupCloseButtonElement);
 
+function openPopup(popup) {
+    popup.classList.add("popup_opened");
+}
 
-function createCard(item) {                                // (item) Берём каждый элемент массива
-    const cardTemplate = templateElement.cloneNode(true);  // клонируем элемент вместе со всем его содержимым
-    const elementsPhoto = cardTemplate.querySelector(".elements__photo");      //Сохранили в новую переменную классы карточек
+function closePopup(popup) {
+    popup.classList.remove("popup_opened");
+    
+} 
+
+function createCard(item) {                               
+    const cardTemplate = templateElement.cloneNode(true);
+    const elementsPhoto = cardTemplate.querySelector(".elements__photo");
     const likeButton = cardTemplate.querySelector(".elements__like");
     const textElement = cardTemplate.querySelector(".elements__text");
     const deleteButtonElement = cardTemplate.querySelector(".elements__delete-button");
 
-    elementsPhoto.src = item.link;                             // наполняем содержимым попап карточек
-    elementsPhoto.alt = item.name;
+    elementsPhoto.src = item.link;
+    elementsPhoto.alt = item.name
     textElement.textContent = item.name;
 
-    likeButton.addEventListener ("click", (evt) => {          // Повешали на сердечко  обработчик клика:методу адвентлист передали функцию-обработчик. Параметр эвент содержит информацию и о произошедшем событии, и о «кликнутом» элементе
-        evt.target.classList.toggle("elements__like_active"); //при клике на сердечко будет добавляться класс активного
+    likeButton.addEventListener ("click", (evt) => {
+        evt.target.classList.toggle("elements__like_active");
     });
 
-    deleteButtonElement.addEventListener ("click", (evt) => { //удаление карточки
+    deleteButtonElement.addEventListener ("click", (evt) => {
         evt.target.closest(".elements__card").remove();
   });
 
@@ -90,15 +98,6 @@ function createCard(item) {                                // (item) Берём 
   return cardTemplate;
 
 }
-
-function openPopup(popup) {
-    popup.classList.add("popup_opened");
-}
-
-function closePopup(popup) {
-    popup.classList.remove("popup_opened");
-    
-} 
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -125,17 +124,15 @@ initialCards.forEach((item) => {
   });
 
 
-
 popupOpenButtonProfile.addEventListener('click', () => {
     openPopup(popupProfile);
     popupName.value = profileName.textContent;
-    popupDescription.value = profileDescription.textContent;  //чтобы имя отображлось в форме, если удалить эти 2 строчки, то в форме будет "Имя" и "О себе"
+    popupDescription.value = profileDescription.textContent;
 })
 
 popupOpenButtonCard.addEventListener("click", () => {
     openPopup(popupCard);
   });
-
 
 popupCloseButtonElement.forEach((button) => {
     const popup = button.closest(".popup");
